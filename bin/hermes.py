d5ef4c456790f928fff9da29d44456da81f5665b
Christophe Fauchard <christophe.fauchard@gmail.com>
2016-05-20 18:04:46 +0200

    first fonctionnal version for download and upload directories by sftp and private key

--- bin/hermes.py
+++ bin/hermes.py
@@ -10,30 +10,96 @@
 
 import sys
 import pypath
-
+import os
 import hermes
 import zeus
 import argparse
 import re
 import stat
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file", help="hermes config file")
-args = parser.parse_args()
+#
+# global vars declaration
+#
+exclude_regex = None
+include_regex = None
 
-try:
-    print("version zeus: " + zeus.__version__)
-    print("version hermes: " + hermes.__version__)
-    parser = zeus.ConfigParser(args.file)
-    
-    print("hermes config file: " + parser.file_name)
-    if parser.get('hermes', 'activation') != 'yes':
-        exit("transfer not activated, aborted")
-    
-    #
-    # SFTP connection
-    #
-    if parser.has_section('sftp'):
+#
+# download function
+#
+# parameters:
+# - file_name: file name to download
+# 
+def sftp_get_file(connection, file):
+    print("download", file, ": size", connection.get_size(file))
+    connection.get(file)
+
+#
+# get command for sftp connection
+#
+def sftp_get(connection):
+
+        #
+        # List files in remote directory
+        #
+        list_files = connection.list()
+        for file in list_files:
+        
+            #
+            # test exclude regex
+            #
+            if exclude_regex and exclude_regex.match(file):
+                print("WARNING: ", file, "excluded by excluderegex")
+            else:
+
+                #
+                # test if directory
+                #
+                if connection.is_dir(file):
+                    print("WARNING:", file, "directory excluded")
+                else:
+                    
+                    #
+                    # test include regex
+                    #
+                    if include_regex:
+                        if include_regex.match(file):
+                            sftp_get_file(connection, file)
+                        else:
+                            print("WARNING:", file, "excluded by includeregex")
+                    else:
+                        sftp_get_file(connection, file)
+
+#
+# upload function
+#
+# parameters:
+# - file_name: file name to upload
+# 
+def sftp_put_file(connection, file):
+    print("upload", file, ": size", os.path.getsize(file))                    
+                        
+#
+# put command for sftp connection
+#
+def sftp_put(connection):
+
+        #
+        # List files in local directory
+        #
+        for file in os.listdir():
+        
+            #
+            # exclude directories
+            #
+            if os.path.isdir(file):
+                print("WARNING: ", file, "directory excluded")
+            else:
+                sftp_put_file(connection, file)
+                        
+#
+# SFTP protocol
+#
+def sftp():
         print("sftp protocol")
         print("sftp connexion to",
             parser.get('hermes', 'user') + "@" + parser.get('hermes', 'host'),
@@ -41,6 +107,10 @@ try:
             parser.get('sftp', 'private_key'),
             "..."
             )
+            
+        #
+        # create an hermes.SFTPConnection object
+        #
         connection = hermes.SFTPConnection(
             parser.get('hermes', 'host'),
             parser.get('hermes', 'user'), 
@@ -52,31 +122,85 @@ try:
         #
         if parser.has_option('hermes', 'excluderegex'):
             exclude_regex = re.compile(parser.get('hermes', 'excluderegex'))
+        else:
+            exclude_regex = None
 
         if parser.has_option('hermes', 'includeregex'):
-            exclude_regex = re.compile(parser.get('hermes', 'includeregex'))
-            
-        #
-        # List files in directory
-        #
-        list_files = connection.list()
-        for file in list_files:
-            if exclude_regex.match(file):
-                print("WARNING: ", file, "excluded")
-            else:
-                if connection.is_dir(file):
-                    print("WARNING:", file, "directory")
-                else:
-                    print(file, ": size", connection.get_size(file))
+            include_regex = re.compile(parser.get('hermes', 'includeregex'))
+        else:
+            include_regex = None
         
+        #
+        # change local directory
+        #
+        if parser.has_option('hermes', 'remotedir'):
+            print("local chdir to", parser.get('hermes', 'localdir'))
+            os.chdir(parser.get('hermes', 'localdir'))        
+        
+        #
+        # change remote directory
+        #
+        if parser.has_option('hermes', 'remotedir'):
+            print("remote chdir to", parser.get('hermes', 'remotedir'))
+            connection.chdir(parser.get('hermes', 'remotedir'))
+        
+        #
+        # download files
+        #
+        if parser.get('hermes', 'command') == 'get':
+            sftp_get(connection)
+        elif parser.get('hermes', 'command') == 'put':
+            sftp_put(connection)        
+        else:
+            print("ERROR: no supported command")
+        
+        #
+        # closing connection
+        #
         print("closing sftp connexion...")
         connection.close()
         print("sftp connexion closed...OK")
-    else: 
-        print("default protocol: ftp")
-    
-except hermes.AuthenticationException as error:
-        print("Authentication private key", username, private_key,"...ERROR")
-except zeus.exception.InvalidConfigurationFileException:
-    print("fichier de configuration " + args.file + " invalide")
 
+
+#
+# command line parsing
+#
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help="hermes config file")
+args = parser.parse_args()
+    
+try:
+
+    #
+    # display version and config informations
+    #
+    print("version zeus: " + zeus.__version__)
+    print("version hermes: " + hermes.__version__)
+    print("hermes config file: " + args.file)
+    
+    #
+    # parse the hermes configuration file
+    #
+    parser = zeus.ConfigParser(args.file)
+    
+    #
+    # exit if activation = no
+    #
+    if parser.get('hermes', 'activation') != 'yes':
+        exit("transfer not activated, aborted")
+    
+    #
+    # SFTP connection
+    #
+    if parser.get('hermes', 'protocol') == "sftp":
+        sftp()
+    else: 
+        print("protocol unsupported", parser.get('hermes', 'protocol'))
+ 
+#
+# exceptions tracking
+#
+except hermes.AuthenticationException as error:
+    print("Authentication private key", username, private_key,"...ERROR")
+except zeus.exception.InvalidConfigurationFileException:
+    print("invalid configuration file: " + args.file)
--- hermes/sftp.py
+++ hermes/sftp.py
@@ -15,8 +15,28 @@ import paramiko, sys
 from hermes.exception import AuthenticationException
 
 class SFTPConnection():
+
     """
     SFTP Class based on Paramiko
+    
+    Attributes:
+    - host
+    - port
+    - username
+    - password
+    - private_key_file
+    - sftp: paramiko.SFTPClient object
+    - transport: paramiko.Transport object
+    
+    Methods:
+    - chdir(dir)
+    - list()
+    - bool is_dir(path)
+    - int get_size(path)
+    - get(remotepath, localpath=None)
+    - put(localpath, remotepath=None)
+    - remove(path)
+    - close()
     """
 
     def __init__(self, host, username, port=22, password=None, private_key=None):
@@ -27,47 +47,102 @@ class SFTPConnection():
         self.private_key_file = private_key
         self.sftp = None
 
+        #
+        # Open SFTP connection
+        #
         try:
+            
+            #
+            # open transport connection
+            #
             self.transport = paramiko.Transport((host, port))
 
+            #
+            # authentication by private key
+            #
             if ( self.private_key_file != None ):
                 self.private_key = paramiko.RSAKey.from_private_key_file(self.private_key_file)
                 self.transport.connect(username=self.username, pkey=self.private_key)
+            
+            #
+            # authentication by login/password
+            #
             elif ( self.password != None ):
                 self.transport.connect(username=self.username, password=self.password)
             else:
                 raise AuthenticationException(self.username, self.private_key_file) 
                 
             self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+            
         except paramiko.ssh_exception.AuthenticationException as error:
             raise AuthenticationException(self.username, self.private_key_file) from error
 
+    #
+    # chdir
+    #
+    def chdir(self, dir):
+        if self.sftp == None:
+            return(None)
+        
+        return(self.sftp.chdir(dir))        
+            
+    #
+    # list files
+    #
     def list(self):
         if self.sftp == None:
             return(None)
         
         return(self.sftp.listdir())
     
-    def is_dir(self, file_name):
-        st = self.sftp.lstat(file_name)
+    #
+    # test if the remote file is a directory
+    #
+    def is_dir(self, path):
+        st = self.sftp.lstat(path)
         if stat.S_ISDIR(st.st_mode):
             return(True)
         else:
             return(False)
     
-    def get_size(self, file_name):
-        st = self.sftp.lstat(file_name)
+    #
+    # get the remote file size
+    #
+    def get_size(self, path):
+        if self.is_dir(path):
+            return(0)
+    
+        st = self.sftp.lstat(path)
         return(st.st_size)
 
+    #
+    # download a file
+    #
     def get(self, remotepath, localpath=None):
         if localpath == None:
             localpath = remotepath
 
-        try:
-            self.sftp.get(remotepath, localpath)           
-        except FileNotFoundError:
-            raise(FileNotFoundException(remotepath))
+        self.sftp.get(remotepath, localpath)           
+ 
+    #
+    # upload a file
+    #
+    def put(self, localpath, remotepath=None):
+        if remotepath == None:
+            remotepath = localpath
 
+        self.sftp.put(localpath, remotepath)           
+                        
+    #
+    # remove remote path
+    #
+    def remove(self, path):
+        if not self.is_dir(path):
+            self.sftp.remove(path)
+            
+    #
+    # close the connection
+    #
     def close(self):   
         if self.transport.is_active():
             self.sftp.close()
